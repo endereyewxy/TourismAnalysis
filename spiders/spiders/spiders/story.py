de979079fe47922e374c264eb34dd9b92a8c5293
endereye <endereyewxy@gmail.com>
2021-07-14 14:54:43 +0800

    移除无用代码

--- spiders/spiders/spiders/story.py
+++ spiders/spiders/spiders/story.py
@@ -21,20 +21,12 @@ class StorySpider(scrapy.Spider):
 
     def __init__(self):
         super().__init__()
-        self.scenic_last_story = 0
-        self.scenic_city_count = 0
         self.cs_set = set()
         self.oi_set = set()
-        self.story_set = set()
         self.page = 2
         self.month_tag = ["&month=1_2_3", "&month=4_5_6", "&month=7_8_9", "&month=10_11_12"]
         self.days_tag = ["&days=1_2_3", "&days=4_5_6_7", "&days=8to10", "&days=11to15", "&days=16tom"]
         self.avg_price_tag = ["&avgPrice=1", "&avgPrice=2", "&avgPrice=3", "&avgPrice=4", "&avgPrice=5"]
-        self.actor_type_tag = ["&actorType=1", "&actorType=2", "&actorType=3", "&actorType=4", "&actorType=5",
-                               "&actorType=6",
-                               "&actorType=7"]
-        self.trip_type_tag = ["", "26", "24", "15", "27", "16", "36", "10", "19", "6", "9", "3", "20", "21", "1", "2",
-                              "4", "5", "7", "8", "11", "12", "13", "17", "18", "14", "22", "23", "39", "29"]
 
     def start_requests(self):
         for month in self.month_tag:
