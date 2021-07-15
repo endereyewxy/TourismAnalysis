a4a671f56b91d9e2a8245270f0e37bf3463582e2
endereye <endereyewxy@gmail.com>
2021-07-15 10:05:59 +0800

    添加连接配置

--- servers/main.py
+++ servers/main.py
@@ -3,7 +3,12 @@ import random
 
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
+from settings import SPARK_URL
+
+if SPARK_URL is None:
+    spark = SparkSession.builder.getOrCreate()
+else:
+    spark = SparkSession.builder.master(SPARK_URL).getOrCreate()
 
 if __name__ == '__main__':
     def foo(_):
@@ -12,7 +17,7 @@ if __name__ == '__main__':
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
 
-    total = spark.sparkContext.parallelize(range(1, 2 * 10000 + 1), 2).map(foo).reduce(operator.add)
-    print(0.0002 * total)
+    total = spark.sparkContext.parallelize(range(1, 2 * 100000 + 1), 2).map(foo).reduce(operator.add)
+    print(0.00002 * total)
 
 spark.stop()
