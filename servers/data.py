b0bfc619d89ebfe2e85f6e7e226c24955e4a8c3e
endereye <endereyewxy@gmail.com>
2021-07-15 16:41:48 +0800

    改进架构

--- servers/data.py
+++ servers/data.py
@@ -2,17 +2,17 @@ from pyspark.sql import SparkSession, functions
 
 import settings
 
-spark = SparkSession.builder \
-    .master(settings.SPARK_URL).config('spark.jars.packages', 'org.xerial:sqlite-jdbc:3.34.0').getOrCreate()
 
-
-class _D:
+class D:
     month_avg_people = None
 
     relp_avg_cost = None
 
 
 def update_raw_database(database: str):
+    spark = SparkSession.builder \
+        .master(settings.SPARK_URL).config('spark.jars.packages', 'org.xerial:sqlite-jdbc:3.34.0').getOrCreate()
+
     loader = spark.read.format('jdbc')
 
     stories = loader.options(url='jdbc:sqlite:' + database, dbtable='stories', driver='org.sqlite.JDBC').load() \
@@ -28,7 +28,7 @@ def update_raw_database(database: str):
         .withColumn('date_start_m', stories.date_start[1].cast('int')) \
         .withColumn('date_start_d', stories.date_start[2].cast('int'))
 
-    _D.month_avg_people = stories.select('date_start_y', 'date_start_m') \
+    D.month_avg_people = stories.select('date_start_y', 'date_start_m') \
         .groupBy('date_start_y', 'date_start_m') \
         .count() \
         .groupBy('date_start_m') \
@@ -37,13 +37,12 @@ def update_raw_database(database: str):
         .withColumnRenamed('avg(count)', 'people_num') \
         .collect()
 
-    _D.relp_avg_cost = stories.select('relp', 'cost') \
+    D.relp_avg_cost = stories.select('relp', 'cost', 'date_count') \
+        .withColumn('cost', stories.cost / stories.date_count) \
+        .select('relp', 'cost') \
         .groupBy('relp') \
         .avg('cost') \
         .withColumnRenamed('avg(cost)', 'avg_cost') \
         .collect()
 
-
-if __name__ == '__main__':
-    update_raw_database('/home/endereye/Workspace/TourismAnalysis/spiders/db/data.db.050')
-    print(_D.relp_avg_cost)
+    spark.stop()
--- servers/main.py
+++ servers/main.py
@@ -1,23 +1,20 @@
-import operator
-import random
+import flask
 
-from pyspark.sql import SparkSession
+from data import update_raw_database, D
 
-from settings import SPARK_URL
+app = flask.Flask('tourism-analysis')
 
-if SPARK_URL is None:
-    spark = SparkSession.builder.getOrCreate()
-else:
-    spark = SparkSession.builder.master(SPARK_URL).getOrCreate()
-
-if __name__ == '__main__':
-    def foo(_):
-        x = 2 * random.random() - 1
-        y = 2 * random.random() - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+update_raw_database('/home/endereye/Workspace/TourismAnalysis/spiders/db/data.db.050')
 
 
-    total = spark.sparkContext.parallelize(range(1, 2 * 100000 + 1), 2).map(foo).reduce(operator.add)
-    print(0.00002 * total)
+@app.route('/<htm>.html')
+def handle_htm(htm: str):
+    return flask.render_template(htm.lstrip('/') + '.html')
 
-spark.stop()
+
+@app.route('/api/<api>')
+def handle_api(api: str):
+    try:
+        return {'error_code': 100, 'data': [row.asDict() for row in getattr(D, api)]}
+    except AttributeError:
+        flask.abort(404)
