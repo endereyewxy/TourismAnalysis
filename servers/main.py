96d2281b0fd35c01df69a9ae63552661b3beeff9
endereye <endereyewxy@gmail.com>
2021-07-15 09:57:56 +0800

    添加测试程序

--- servers/main.py
+++ servers/main.py
@@ -1,2 +1,18 @@
+import operator
+import random
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
 if __name__ == '__main__':
-    pass
+    def foo(_):
+        x = 2 * random.random() - 1
+        y = 2 * random.random() - 1
+        return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+
+    total = spark.sparkContext.parallelize(range(1, 2 * 10000 + 1), 2).map(foo).reduce(operator.add)
+    print(0.0002 * total)
+
+spark.stop()
