b552d7bd00763310758c2ad9dbd86df57b8d6e90
endereye <endereyewxy@gmail.com>
2021-07-16 11:48:06 +0800

    实现大部分API

--- servers/data.py
+++ servers/data.py
@@ -1,5 +1,37 @@
 from pyspark.sql import SparkSession, functions
 
+provinces = ['四川',
+             '浙江',
+             '福建',
+             '江苏',
+             '湖南',
+             '山东',
+             '安徽',
+             '广东',
+             '河北',
+             '湖北',
+             '吉林',
+             '上海',
+             '江西',
+             '广西',
+             '贵州',
+             '北京',
+             '云南',
+             '重庆',
+             '河南',
+             '陕西',
+             '山西',
+             '辽宁',
+             '新疆',
+             '内蒙古',
+             '黑龙江',
+             '天津',
+             '甘肃',
+             '海南',
+             '青海',
+             '宁夏',
+             '西藏']
+
 
 class D:
     month_avg_people = None
@@ -10,6 +42,10 @@ class D:
 
     play_way_percent_topn = None
 
+    prov_hot = None
+
+    city_everypeolple_avg_cost = None
+
 
 def update_raw_database(database: str):
     spark = SparkSession.builder.config('spark.jars.packages', 'org.xerial:sqlite-jdbc:3.34.0').getOrCreate()
@@ -17,17 +53,16 @@ def update_raw_database(database: str):
     loader = spark.read.format('jdbc')
 
     stories = loader.options(url='jdbc:sqlite:' + database, dbtable='stories', driver='org.sqlite.JDBC').load() \
-        .withColumn('date_start', functions.split('date_start', r'/'))
+        .withColumn('date_start', functions.split('date_start', r'/')) \
+        .withColumn('date_start_y', functions.col('date_start')[0].cast('int')) \
+        .withColumn('date_start_m', functions.col('date_start')[1].cast('int')) \
+        .withColumn('date_start_d', functions.col('date_start')[2].cast('int')) \
+        .withColumn('hot', functions.col('date_count') * functions.col('text_count'))
     scenics = loader.options(url='jdbc:sqlite:' + database, dbtable='scenics', driver='org.sqlite.JDBC').load()
 
     cities = loader.options(url='jdbc:sqlite:' + database, dbtable='cities', driver='org.sqlite.JDBC').load()
     visits = loader.options(url='jdbc:sqlite:' + database, dbtable='visits', driver='org.sqlite.JDBC').load()
     how_to = loader.options(url='jdbc:sqlite:' + database, dbtable='how_to', driver='org.sqlite.JDBC').load()
-
-    stories = stories \
-        .withColumn('date_start_y', stories.date_start[0].cast('int')) \
-        .withColumn('date_start_m', stories.date_start[1].cast('int')) \
-        .withColumn('date_start_d', stories.date_start[2].cast('int'))
 
     D.month_avg_people = stories.select('date_start_y', 'date_start_m') \
         .groupBy('date_start_y', 'date_start_m') \
@@ -49,18 +84,41 @@ def update_raw_database(database: str):
     D.relp_story_num = stories.select('relp') \
         .groupBy('relp') \
         .count() \
-        .withColumnRenamed('count', 'story_num') \
+        .withColumnRenamed('relp', 'name') \
+        .withColumnRenamed('count', 'value') \
         .collect()
 
     D.play_way_percent_topn = how_to.select('h_tag') \
         .groupBy('h_tag') \
         .count() \
         .withColumnRenamed('h_tag', 'play_way') \
-        .withColumnRenamed('count', 'percent')
-    D.play_way_percent_topn = D.play_way_percent_topn \
-        .withColumn('percent', functions.round(D.play_way_percent_topn.percent / stories.count() * 100).cast('int'))
-    D.play_way_percent_topn = D.play_way_percent_topn \
-        .orderBy(D.play_way_percent_topn.percent.desc()) \
+        .withColumnRenamed('count', 'percent') \
+        .withColumn('percent', functions.round(functions.col('percent') / stories.count() * 100).cast('int')) \
+        .orderBy(functions.col('percent').desc()) \
+        .collect()
+
+    D.prov_hot = stories.select('date_start_y', 'loct', 'hot') \
+        .filter(2019 <= functions.col('date_start_y')) \
+        .filter(2021 >= functions.col('date_start_y')) \
+        .join(cities, functions.col('loct') == cities.name) \
+        .select('date_start_y', 'hot', 'prov') \
+        .filter(functions.col('prov').isin(provinces)) \
+        .groupBy('date_start_y', 'prov') \
+        .sum('hot') \
+        .withColumnRenamed('date_start_y', 'year') \
+        .withColumnRenamed('sum(hot)', 'hot') \
+        .collect()
+
+    D.city_everypeolple_avg_cost = stories.select('date_start_y', 'loct', 'cost') \
+        .filter(2019 <= functions.col('date_start_y')) \
+        .filter(2021 >= functions.col('date_start_y')) \
+        .join(cities, functions.col('loct') == cities.name) \
+        .select('date_start_y', 'prov', 'cost') \
+        .filter(functions.col('prov').isin(provinces)) \
+        .groupBy('date_start_y', 'prov') \
+        .avg('cost') \
+        .withColumnRenamed('date_start_y', 'year') \
+        .orderBy(functions.col('avg(cost)').desc()) \
         .collect()
 
     spark.stop()
--- servers/main.py
+++ servers/main.py
@@ -18,6 +18,16 @@ def handle_api(api: str):
         data = [row.asDict() for row in getattr(D, api)]
         if api == 'play_way_percent_topn':
             data = data[:int(flask.request.args.get('num', ''))]
+        if api == 'prov_hot':
+            new_data = {}
+            for obj in data:
+                new_data.setdefault(obj['year'], []).append({'name': obj['prov'], 'hot': obj['hot']})
+            data = [{'year': year, 'prov': prov} for year, prov in new_data.items()]
+        if api == 'city_everypeolple_avg_cost':
+            new_data = {}
+            for obj in data:
+                new_data.setdefault(obj['year'], []).append({'name': obj['prov'], 'avg_cost': obj['avg(cost)']})
+            data = [{'year': year, 'prov': prov} for year, prov in new_data.items()]
         return {'error_code': 100, 'data': data}
     except AttributeError:
         flask.abort(404)
